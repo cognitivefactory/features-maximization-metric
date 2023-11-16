063bf926738afa0e6b3fc2553df0b8dd65680039
Erwan Schild <erwan.schild@e-i.com>
2023-11-16 11:47:58 +0100

    docs: update __init__.py documentation

--- src/cognitivefactory/features_maximization_metric/__init__.py
+++ src/cognitivefactory/features_maximization_metric/__init__.py
@@ -1,7 +1,14 @@
-"""
-cognitivefactory-features-maximization-metric package.
+# -*- coding: utf-8 -*-
 
-Implementation of Features Maximization Metric, an unbiased metric aimed at estimate the quality of an unsupervised classification.
+"""
+* Name:         cognitivefactory.features_maximization_metric
+* Description:  Implementation of Features Maximization Metric, an unbiased metric aimed at estimate the quality of an unsupervised classification.
+* Author:       Erwan SCHILD
+* Created:      16/02/2023
+* Licence:      CeCILL (https://cecill.info/licences.fr.html)
+
+One module is available:
+- `fmc`: the implementation of Features Maximization Metric. See [features_maximization_metric/utils](https://cognitivefactory.github.io/features-maximization-metric/reference/cognitivefactory/features_maximization_metric/fmc/) documentation.
 """
 
 from typing import List
